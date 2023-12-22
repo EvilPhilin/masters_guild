9c7f6c9c23cda14daea8ff0377d77700a600c45f
EvilPhilin <kurdjaev_sergey@rambler.ru>
2023-12-22 11:06:51 +0200

    Added validation for masters model

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,2 +1,5 @@
 class Master < ApplicationRecord
+	validates :name, :surname, presence: true
+	validates :name, :surname, length: { maximum: 32 }
+	validates :about_me, length: { maximum: 512 }
 end
