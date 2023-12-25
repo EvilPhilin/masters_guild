67c47cedb2a820ae693ac8375930ba08c690f711
EvilPhilin <kurdjaev_sergey@rambler.ru>
2023-12-25 09:42:40 +0200

    1. Refactored masters validation 2. Fixed masters 'edit' view

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,5 +1,5 @@
 class Master < ApplicationRecord
-	validates :name, :surname, presence: true
-	validates :name, :surname, length: { maximum: 32 }
+	validates :name, presence: true, length: { maximum: 32 }
+	validates :surname, presence: true, length: { maximum: 32 }
 	validates :about_me, length: { maximum: 512 }
 end
